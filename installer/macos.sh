72602ca05e39fdcbfcc7261ecc08634f40f88f30
dev.Wladimir <50156678+devWladimir@users.noreply.github.com>
2022-10-13 17:37:13 +0300

    Replaced camelCase to snake_case for correct work sh script

--- installer/macos.sh
+++ installer/macos.sh
@@ -2,10 +2,10 @@
 set -u
 
 # Terminal colors
-terminalColorClear='\033[0m'
-terminalColorRed='\033[1;31m'
-terminalColorGreen='\033[1;32m'
-terminalColorYellow='\033[1;33m'
+terminal_color_clear='\033[0m'
+terminal_color_red='\033[1;31m'
+terminal_color_green='\033[1;32m'
+terminal_color_yellow='\033[1;33m'
 
 # Abort script
 abort() {
@@ -14,101 +14,101 @@ abort() {
 }
 
 # Default message
-echoDefault() {
-    echo -e "${terminalColorClear}$1${terminalColorClear}"
+echo_default() {
+    echo -e "${terminal_color_clear}$1${terminal_color_clear}"
     sleep 2
 }
  
 # Green color message
-echoSuccess() {
-    echo -e "${terminalColorGreen}$1${terminalColorClear}"
+echo_success() {
+    echo -e "${terminal_color_green}$1${terminal_color_clear}"
     sleep 2
 }
  
 # Yellow color message
-echoWarning() {
-    echo -e "${terminalColorYellow}$1${terminalColorClear}"
+echo_warning() {
+    echo -e "${terminal_color_yellow}$1${terminal_color_clear}"
     sleep 2
 }
  
 # Red color message
-echoError() {
-    echo -e "${terminalColorRed}$1${terminalColorClear}"
+echo_error() {
+    echo -e "${terminal_color_red}$1${terminal_color_clear}"
     sleep 2
 }
 
 # Checking installed bash in OS
-checkBash() {
+check_bash() {
     if [ -z "${BASH_VERSION:-}" ]
     then
-        echoError "🌩 Bash is required to interpret this script."
+        echo_error "🌩 Bash is required to interpret this script."
     fi
 }
 
 # Confirm / Abort install
-confirmInstall() {
-    echoDefault ""
-    echoDefault "🔥 This script will install [Xcode Command Line Tools, Homebrew and Chezmoi.io]"
+confirm_install() {
+    echo_default ""
+    echo_default "🔥 This script will install [Xcode Command Line Tools, Homebrew and Chezmoi.io]"
     read -n 1 -r -s -p $'    Press any key to continue or Control+C to abort...\n\n'
 }
 
 # Install Xcode Command Line Tools
-function installCommandLineTools() {
-    echoDefault "⚡️ Looking for Xcode Command Line Tools..."
+function install_command_line_tools() {
+    echo_default "⚡️ Looking for Xcode Command Line Tools..."
 
     if ! [ $(xcode-select -p) ]; then
-        echoError "🌩 You do not have the Xcode Command Line Tools."
-        echoWarning "⚡️ Installing Xcode Command Line Tools..."
+        echo_error "🌩 You do not have the Xcode Command Line Tools."
+        echo_warning "⚡️ Installing Xcode Command Line Tools..."
 
         xcode-select --install
         sudo xcodebuild -license accept
 
-        echoSuccess "☀️ Xcode Command line Tools successfully installed, so go to next step..."
+        echo_success "☀️ Xcode Command line Tools successfully installed, so go to next step..."
     else
-        echoSuccess "☀️ Xcode Command Line Tools are already installed, so skipping to next step..."
+        echo_success "☀️ Xcode Command Line Tools are already installed, so skipping to next step..."
     fi
 }
 
 # Install Homebrew
-function installHomebrew() {
-    echoDefault "⚡️ Looking for Homebrew..."
+function install_homebrew() {
+    echo_default "⚡️ Looking for Homebrew..."
 
     if test ! $(which brew); then
-        echoError "🧨 You do not have the Homebrew."
-        echoWarning "⚡️ Installing Homebrew..."
+        echo_error "🧨 You do not have the Homebrew."
+        echo_warning "⚡️ Installing Homebrew..."
 
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"    
 
-        echoSuccess "☀️ Homebrew successfully installed, so go to next step..."
+        echo_success "☀️ Homebrew successfully installed, so go to next step..."
     else
-        echoSuccess "☀️ Homebrew already installed, so skipping to next step..."
+        echo_success "☀️ Homebrew already installed, so skipping to next step..."
     fi
 }
 
 
 # Install Chezmoi.io 
-function installChezmoi() {
-    echoDefault "⚡️ Looking for Chezmoi..."
+function install_chezmoi() {
+    echo_default "⚡️ Looking for Chezmoi..."
 
     if test ! $(which chezmoi); then
-        echoError "🧨 You do not have the Chezmoi."
-        echoWarning "⚡️ Installing Chezmoi..."
+        echo_error "🧨 You do not have the Chezmoi."
+        echo_warning "⚡️ Installing Chezmoi..."
 
         brew install chezmoi
 
-        echoSuccess "☀️ Chezmoi successfully installed, so go to next step..."
+        echo_success "☀️ Chezmoi successfully installed, so go to next step..."
     else
-        echo "👌 Cezmoi already installed, so skipping to next step..."
+        echo_success "👌 Cezmoi already installed, so skipping to next step..."
     fi
 }
 
 
 # Process installing
-checkBash
-confirmInstall
-installCommandLineTools
-installHomebrew
-installChezmoi
+check_bash
+confirm_install
+install_command_line_tools
+install_homebrew
+install_chezmoi
 
 # Success messsage
 echo ""
